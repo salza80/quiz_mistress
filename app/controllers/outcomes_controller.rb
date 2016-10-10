6b92b5f9ae361e4900c98aafbb73c37db1b9c335
Sally Mclean <smclean17@gmail.com>
2016-10-10 19:09:56 +0200

    fix fb meta tags quizzes and outcomes

--- app/controllers/outcomes_controller.rb
+++ app/controllers/outcomes_controller.rb
@@ -18,7 +18,7 @@ class OutcomesController < ApplicationController
 
     def set_tags(quiz, outcome)
       set_meta_tags title: quiz.title + " | " + outcome.title, description: quiz.description
-      set_meta_tags og:{ type: "article",title: quiz.title, url: request.original_url, description: quiz.description, image: request.base_url + "/assets/images/" + outcome.main_image.path }
+      set_meta_tags og:{ type: "article",title: outcome.title, url: request.original_url, description: outcome.description, image: request.base_url + "/assets/images/" + outcome.main_image.path }
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
