b115d5a6ac3f60fc783d14d016e15c53cd17cd82
Sally Mclean <smclean17@gmail.com>
2016-10-25 14:47:14 +0200

    fix facebook register

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,43 +7,33 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,  :confirmable
   devise :omniauthable, :omniauth_providers => [:facebook]
-  has_many :providers
+  has_many :providers, dependent: :destroy
 
   def self.from_omniauth(auth)
     puts auth
-    register_oauth_with_matching_email(auth)
-    user = find_user_by_provider(auth.provider, auth.uid)
-    if user
-
-    else
+    provider = Provider.find_by(provider:auth.provider, uid: auth.uid)
+    return provider.user if provider
+    user = register_oauth_with_matching_email(auth)
+    unless user
       user = User.new
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-      # user.first_name = auth.info.name   # assuming the user model has a name
-      # user.image = auth.info.image # assuming the user model has an image
+      #user.name = auth.info.name
+      #user.image = auth.info.image # assuming the user model has an image
+      user.skip_confirmation!
       user.save!
-      user.reload
-      register_oauth_with_matching_email(auth)
+      user = register_oauth_with_matching_email(auth)
     end
     user
   end
 
   def self.register_oauth_with_matching_email(auth)
-    user = where("email = :email",email: auth.info.email).first
-    return unless user
-    provider = user.providers.where("provider=:provider and uid=:uid", provider:auth.provider, uid: auth.uid).first
+    user = find_by(email: auth.info.email)
+    return nil unless user
+    provider = user.providers.find_by(provider:auth.provider, uid: auth.uid)
     unless provider
-      p = user.providers.create(uid: auth.uid, provider: auth.provider)
-      # user.uid = auth.uid
-      # user.provider = auth.provider
-      # user.save
+      user.providers.create(uid: auth.uid, provider: auth.provider)
     end
+    user
   end
-
-  def self.find_user_by_provider(provider, uid)
-    provider = Provider.where("provider=:provider and uid=:uid", provider:provider, uid: uid).first
-    return nil unless provider
-    provider.user
-  end
-
 end
