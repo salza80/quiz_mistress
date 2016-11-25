86070666bca69552004bf0cbafdd6e47b53652d2
Sally Mclean <smclean17@gmail.com>
2016-11-25 16:26:23 +0100

    heroku fixes

--- app/assets/javascripts/general.js
+++ app/assets/javascripts/general.js
@@ -1,57 +1,57 @@
+(function() {
+  this.Quiz || (this.Quiz = {});
 
+  Quiz.start = function(){
+    $(".quiz-start").hide();
+    $("#renderer").removeClass('hidden');
+  }
 
-var Quiz = Quiz || {};
+  var General = General || {};
 
-Quiz.start = function(){
-  $(".quiz-start").hide();
-  $("#renderer").removeClass('hidden');
-}
+  General.replaceWithFade = function (selector, replace){
+    $(selector).fadeOut('slow', function(){
+      $(this).replaceWith($(replace))
+      $(window).scrollTo($(this));
+      $(selector).fadeIn('slow');
+    });
+  }
 
-var General = General || {};
+  General.replaceContentWithFade = function(selector, replace){
+    $(selector).fadeOut('slow', function(){
+      $(this).html($(replace))
+      $(window).scrollTo($(this));
+      $(selector).fadeIn('slow');
+    });
+  }
 
-General.replaceWithFade = function (selector, replace){
-  $(selector).fadeOut('slow', function(){
-    $(this).replaceWith($(replace))
-    $(window).scrollTo($(this));
-    $(selector).fadeIn('slow');
-  });
-}
+  General.doFormErrors = function(){
+    var e = $("#errors")
+    jQuery.each(e.data("errors"), function(index){
+     General.showFormErrors(this.model,e.data("form-selector"),jQuery.parseJSON(this.errors))
+    });
+  }
 
-General.replaceContentWithFade = function(selector, replace){
-  $(selector).fadeOut('slow', function(){
-    $(this).html($(replace))
-    $(window).scrollTo($(this));
-    $(selector).fadeIn('slow');
-  });
-}
-
-General.doFormErrors = function(){
-  var e = $("#errors")
-  jQuery.each(e.data("errors"), function(index){
-   General.showFormErrors(this.model,e.data("form-selector"),jQuery.parseJSON(this.errors))
-  });
-}
-
-  
-General.showFormErrors=function(modelName, formSelector, errors){
-  var form = $(formSelector)
-  $.each(errors, function(field, messages){   
-    if(messages.length == 0){return true;}
-    var input = form.find('input, select, textarea').filter(function(){
-      var name = $(this).attr('name')
-      if(name !==undefined){
-        return name.match(new RegExp(modelName + '\\[' + field + '\\(?'))
-      }
+    
+  General.showFormErrors=function(modelName, formSelector, errors){
+    var form = $(formSelector)
+    $.each(errors, function(field, messages){   
+      if(messages.length == 0){return true;}
+      var input = form.find('input, select, textarea').filter(function(){
+        var name = $(this).attr('name')
+        if(name !==undefined){
+          return name.match(new RegExp(modelName + '\\[' + field + '\\(?'))
+        }
+      })
+      if(input.length ==0){return form;}
+      input.closest('.form-group').addClass('has-warning')
+      input.addClass('form-control-warning')
+      $.each(messages, function(index,message){
+        input.parent().append($("<div class='form-control-feedback'></div>").html(message))
+      })
     })
-    if(input.length ==0){return form;}
-    input.closest('.form-group').addClass('has-warning')
-    input.addClass('form-control-warning')
-    $.each(messages, function(index,message){
-      input.parent().append($("<div class='form-control-feedback'></div>").html(message))
-    })
-  })
-  return form
-}
+    return form
+  }
+}).call(this);
 
 
 
--- app/assets/javascripts/utility.js
+++ app/assets/javascripts/utility.js
@@ -1,9 +1,12 @@
+(function() {
+  
+  this.Util || (this.Util = {});
 
-var Util = Util || {};
-Util.escapeRegExp = function(str) {
-    return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
-}
+  Util.escapeRegExp = function(str) {
+      return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+  }
 
-Util.replaceAll = function(str, find, replace) {
-  return str.replace(new RegExp(this.escapeRegExp(find), 'g'), replace);
-}
+  Util.replaceAll = function(str, find, replace) {
+    return str.replace(new RegExp(this.escapeRegExp(find), 'g'), replace);
+  }
+}).call(this);
