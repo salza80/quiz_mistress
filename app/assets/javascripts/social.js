4fdc2215524a864dda2961b5b162f18ac7933a7a
Sally Mclean <smclean17@gmail.com>
2016-11-25 19:49:18 +0100

    fix js error

--- app/assets/javascripts/social.js
+++ app/assets/javascripts/social.js
@@ -1,13 +1,12 @@
-
 (function() {
   this.Social || (this.Social = {});
   Social.share = function(btn){
     FB.ui({
-    method: 'share',
-    href: $(btn).data('s-href'),
-    redirect_url: $(btn).data('s-redirect-url'),
-  }, function(response){
-    console.log(response)
-  });
+      method: 'share',
+      href: $(btn).data('s-href'),
+      redirect_url: $(btn).data('s-redirect-url'),
+    }, function(response){
+      console.log(response)
+    });
   }
 }).call(this);
--- app/assets/javascripts/utility.js
+++ app/assets/javascripts/utility.js
@@ -1,4 +1,3 @@
-console.log("util")
 (function() {
   this.Util || (this.Util = {});
 
