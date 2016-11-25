fe847973730d85ff4f879d19b20ece57737faa1b
Sally Mclean <smclean17@gmail.com>
2016-11-25 17:48:54 +0100

    debug test

--- app/assets/javascripts/utility.js
+++ app/assets/javascripts/utility.js
@@ -1,12 +1,9 @@
-(function() {
-  
-  this.Util || (this.Util = {});
+console.log("util")
+var Util = Util || {};
+Util.escapeRegExp = function(str) {
+    return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+}
 
-  Util.escapeRegExp = function(str) {
-      return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
-  }
-
-  Util.replaceAll = function(str, find, replace) {
-    return str.replace(new RegExp(this.escapeRegExp(find), 'g'), replace);
-  }
-}).call(this);
+Util.replaceAll = function(str, find, replace) {
+  return str.replace(new RegExp(this.escapeRegExp(find), 'g'), replace);
+}
