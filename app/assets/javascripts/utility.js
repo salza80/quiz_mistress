3c12aff6cdce63dc93a01148f63a2a096bca76dd
Sally Mclean <smclean17@gmail.com>
2017-03-20 17:42:43 +0100

    touch

--- app/assets/javascripts/utility.js
+++ app/assets/javascripts/utility.js
@@ -43,7 +43,7 @@ $(document).on('turbolinks:load', PageEvents.load)
         isTouch = true
         if (curRootClass != 'can-touch'){ //add "can-touch' class if it's not already present
             curRootClass = 'can-touch'
-            document.documentElement.classList.add(curRootClass)
+            $(document).addClass(curRootClass)
         }
         isTouchTimer = setTimeout(function(){isTouch = false}, 500) //maintain "istouch" state for 500ms so removetouchclass doesn't get fired immediately following a touch event
     }
@@ -52,11 +52,11 @@ $(document).on('turbolinks:load', PageEvents.load)
         if (!isTouch && curRootClass == 'can-touch'){ //remove 'can-touch' class if not triggered by a touch event and class is present
             isTouch = false
             curRootClass = ''
-            document.documentElement.classList.remove('can-touch')
+             $(document).removeClass('can-touch'))
         }
     }
 
-    document.addEventListener('touchstart', addtouchclass, false) //this event only gets called when input type is touch
-    document.addEventListener('mouseover', removetouchclass, false) //this event gets called when input type is everything from touch to mouse/ trackpad
+    $(document).on('touchstart', addtouchclass) //this event only gets called when input type is touch
+    $(document).on('mouseover', removetouchclass) //this event gets called when input type is everything from touch to mouse/ trackpad
 })();
 
