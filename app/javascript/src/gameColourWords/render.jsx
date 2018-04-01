d763b65ac6ce59b5f59b889e20a526a5c6b27fa6
Sally Mclean <smclean17@gmail.com>
2018-04-01 16:35:49 +0200

    fix game for react16

--- app/javascript/src/gameColourWords/render.jsx
+++ app/javascript/src/gameColourWords/render.jsx
@@ -2,12 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Game from './components/game.jsx'
 
-document.addEventListener('DOMContentLoaded', () => {
-  const node = document.getElementById('renderer')
-  ReactDOM.render(
-    <Game />,
-    node
-  )
-})
+const node = document.getElementById('renderer')
+ReactDOM.render(
+  <Game />,
+  node
+)
+
 
 
--- app/javascript/src/quiz/render.jsx
+++ app/javascript/src/quiz/render.jsx
@@ -2,13 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Quiz from './components/quiz.jsx'
 
-document.addEventListener('DOMContentLoaded', () => {
-  const node = document.getElementById('renderer')
-  const props = JSON.parse(node.getAttribute('data-props'))
-  ReactDOM.render(
-    <Quiz {...props} />,
-    node
-  )
-})
-
-
+const node = document.getElementById('renderer')
+const props = JSON.parse(node.getAttribute('data-props'))
+ReactDOM.render(
+  <Quiz {...props} />,
+  node
+)
